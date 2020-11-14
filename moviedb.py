7b54b99811cea08b28efd42b6d5e717e1f6869d8
Anisha Tripathi <anishatripathi@anishas-air.lan>
2020-11-14 12:02:20 -0800

    complete api

--- moviedb.py
+++ moviedb.py
@@ -6,11 +6,25 @@ import urllib.request
 load_dotenv()
 
 MOVIE_API_KEY = os.getenv('API_KEY')
-print(MOVIE_API_KEY)
 
 def build_search(search_query: str):
     part_url = 'http://api.themoviedb.org/3/search/movie?api_key=' + MOVIE_API_KEY + '&query=' + search_query
-    print(part_url)
     return part_url
 
-build_search('avengers')
+def get_results(url: str):
+    response = None
+    try:
+        json_text = response.read().decode(encoding = 'utf-8')
+        return json.loads(json)
+    finally:
+        if response != None:
+            response.close()
+
+def get_pop_titles(json_result: dict):
+    pop_titles = json_result['results']
+    selected = pop_titles[0]
+    first_choice = selected['title']
+    movie_id = selected['id']
+    movie_poster = selected['poster_path']
+    movie_lang = selected['original_language']
+
