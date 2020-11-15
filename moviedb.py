d8c04c30b5c48be134ca1f80ddb052b38b4d5848
Brian Tu <briant2488@gmail.com>
2020-11-14 16:44:50 -0800

    response changes

--- moviedb.py
+++ moviedb.py
@@ -14,8 +14,9 @@ def build_search(search_query: str):
 def get_results(url: str):
     response = None
     try:
+        response = urllib.request.urlopen(url)
         json_text = response.read().decode(encoding = 'utf-8')
-        return json.loads(json)
+        return json.loads(json_text)
     finally:
         if response != None:
             response.close()
